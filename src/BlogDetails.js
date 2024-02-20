b58ef52e8577e92e34175cbb7053951740ad266b
Rhythm-08 <rhythmsharma808@gmail.com>
2024-02-20 21:57:38 +0530

    feat: Learned forms and 404 route and post and delete api

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,35 @@
 import { useParams } from 'react-router-dom';
+import  useFetch  from './useFetch';
+import { useNavigate } from 'react-router-dom';
+
 
 const BlogDetails = () => {
 
     const { id } = useParams();
+    const {data ,error , isPending } = useFetch('http://localhost:8000/blogs/'+id);
+    const navigate = useNavigate();
+
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/'+data.id,{
+        method:'DELETE'
+        }).then(() => {
+            navigate("/");
+        })
+    }
 
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            { isPending && <div>Loading ... </div>}
+            { error && <div>{ error }</div> }
+            { data && (
+                <article>
+                    <h2>{ data.title }</h2>
+                    <p>Written By: { data.author }</p>
+                    <div>{ data.body }</div>
+                    <button onClick={ handleClick }>Delete</button>
+                </article>
+            ) }
         </div>
      );
 }
